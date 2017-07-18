45c1118fa5675d27d74bd0d3f7757a661d4a8552
vanncho <van_cho@abv.bg>
2017-07-18 18:09:48 +0300

    Created functionality for roll and fix bugs on skeleton

--- commands/roll.js
+++ commands/roll.js
@@ -1,4 +1,28 @@
 define ([], function () {
 
+    function roll(array, commands) {
 
+        if (commands.length === 1 || commands.length > 2) {
+            throw new Error('Invalid parameters!');
+        }
+
+        let rollDirection = commands[1];
+
+        if (rollDirection !== 'left' && rollDirection !== 'right') {
+            throw new Error('Error: invalid command parameters');
+        }
+
+        let currArrElem;
+        if (rollDirection === 'left') {
+            currArrElem = array.shift();
+            array.push(currArrElem);
+        } else {
+            currArrElem = array.pop();
+            array.unshift(currArrElem);
+        }
+    }
+
+    return {
+        roll
+    }
 });
--- console-reader.js
+++ console-reader.js
@@ -13,10 +13,10 @@ define (["./commands/reverse",
     let terminal = $('#display');
     let inputCommands = $('#input-commands');
     $('#btn-enter').on('click', submit);
-    inputCommands.on('keypress', (e) => e.which == 13 ? submit() : '');
+    inputCommands.on('keypress', (e) => e.which === 13 ? submit() : '');
 
     function submit() {
-        let commandTokens = inputCommands.val().split(' ').filter((e) => e != ' ');
+        let commandTokens = inputCommands.val().split(' ').filter((e) => e !== ' ');
         if(!initialized){
             theArray = commandTokens.slice(0);
             terminal.val(theArray.join(' ') + '\n');
@@ -25,36 +25,46 @@ define (["./commands/reverse",
             return;
         }
 
-        switch(commandTokens[0]){
-            case 'reverse':
-                let result = reverseArr.reverseArr(theArray, commandTokens);
-                if(Array.isArray(result)){
-                    theArray = result.slice(0);
-                    terminal.val(terminal.val() + theArray.join(' ') + '\n');
-                } else {
-                    terminal.val(terminal.val() + result + '\n');
-                }
-                inputCommands.val('');
-                break;
-            case 'append':
-                break;
-            case 'prepend':
-                break;
-            case 'insert':
-                break;
-            case 'delete':
-                break;
-            case 'roll':
-                break;
-            case 'sort':
-                break;
-            case 'count':
-                break;
-            case 'end':
-                break;
+        let result;
 
-            default: terminal.val(terminal.val() + 'Error: Invalid command!' + '\n');
-                inputCommands.val('');
+        try {
+            switch(commandTokens[0]){
+                case 'reverse':
+                    result = reverseArr.reverseArr(theArray, commandTokens);
+                    if(Array.isArray(result)){
+                        theArray = result.slice(0);
+                        terminal.val(terminal.val() + theArray.join(' ') + '\n');
+                    } else {
+                        terminal.val(terminal.val() + result + '\n');
+                    }
+                    //inputCommands.val(''); // down after the switch, avoid code repeating
+                    break;
+                case 'append':
+                    break;
+                case 'prepend':
+                    break;
+                case 'insert':
+                    break;
+                case 'delete':
+                    break;
+                case 'roll': // TODO
+                    result = rollArr.roll(theArray, commandTokens);
+                    terminal.val(terminal.val() + theArray.join(' ') + '\n');
+                    break;
+                case 'sort':
+                    break;
+                case 'count':
+                    break;
+                case 'end':
+                    break;
+
+                default: terminal.val(terminal.val() + 'Error: Invalid command!' + '\n');
+                //inputCommands.val(''); // down after the switch, avoid code repeating
+            }
+        } catch (err) {
+            terminal.val(terminal.val() + err.message + '\n');
         }
+
+        inputCommands.val('');
     }
 }));
