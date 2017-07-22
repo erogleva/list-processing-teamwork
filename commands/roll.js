48b55dbf51d8b1e56e033233b1cc5a8a27bb6e18
vanncho <van_cho@abv.bg>
2017-07-22 18:49:52 +0300

    Fix Roll command to work with Custom Error

--- commands/roll.js
+++ commands/roll.js
@@ -1,21 +1,23 @@
-define (["../errors/input-error"], function (inputError) {
+define(["../errors/command-parameter-error"], function (InputError) {
 
     function roll(array, commands) {
 
         if (commands.length === 1 || commands.length > 2) {
-            throw new inputError.InputError();
+            throw new InputError();
         }
 
         let rollDirection = commands[1];
 
         if (rollDirection !== 'left' && rollDirection !== 'right') {
-            throw new inputError.InputError();
+            throw new InputError();
         }
 
         let currArrElem;
+
         if (rollDirection === 'left') {
             currArrElem = array.shift();
             array.push(currArrElem);
+            
         } else {
             currArrElem = array.pop();
             array.unshift(currArrElem);
