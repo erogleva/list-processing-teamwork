335d074a27385230db7eecf4156fc48f25457518
vanncho <van_cho@abv.bg>
2017-07-22 18:43:33 +0300

    Fix Delete command to work with Custom Error

--- commands/delete.js
+++ commands/delete.js
@@ -1,13 +1,15 @@
-define (["../errors/input-error"], function (inputError) {
+define(["../errors/command-parameter-error"], function (InputError) {
 
     function deleteArr(arr, commandTokens) {
+        
         if(commandTokens.length !== 2){
-            throw new inputError.InputError();
+            throw new InputError();
         }
 
         let index = commandTokens[1];
+
         if(index < 0 || index >= arr.length){
-            throw new inputError.InputError(`Error: Invalid index ${index}!`);
+            throw new InputError(`Error: Invalid index ${index}!`);
         }
 
         let theArray = arr.splice(index,1);
