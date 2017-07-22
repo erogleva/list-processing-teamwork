cc71fc70fad73eb1a53186ff4b47964548e0de62
vanncho <van_cho@abv.bg>
2017-07-22 18:54:57 +0300

    Fix name of error

--- errors/input-command-error.js
+++ errors/input-command-error.js
@@ -1,9 +1,9 @@
 define ([], function () {
 
-    return function InputError(message) {
+    return function InputCommandError(message) {
         this.name = this.constructor.name;
         this.message = message || "Error: Invalid command!";
-        InputError.prototype = Error.prototype;
+        InputCommandError.prototype = Error.prototype;
     }
 
 });
