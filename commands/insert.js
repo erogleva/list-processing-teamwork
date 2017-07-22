e8af529f10ed4dd6bf8a03e102f446c2f35c05bd
vanncho <van_cho@abv.bg>
2017-07-22 18:45:22 +0300

    Fix Insert command to work with Custom Error

--- commands/insert.js
+++ commands/insert.js
@@ -1,13 +1,17 @@
-define (["../errors/input-error"], function (inputError) {
-    function insertArr(arr,commandTokens) {
-        if(commandTokens.length!=3){
-            throw new inputError.InputError();
+define(["../errors/command-parameter-error"], function (InputError) {
+
+    function insertArr(arr, commandTokens) {
+
+        if(commandTokens.length !== 3){
+            throw new InputError();
         }
         let index = Number(commandTokens[1]);
-        if(index>=0&&index<=arr.length-1){
+
+        if(index >= 0 && index <= arr.length - 1) {
             return arr.splice(index, 0, commandTokens[2]);
         }
-        throw new inputError.InputError(`Error: invalid index ${index}`);
+
+        throw new InputError(`Error: invalid index ${index}`);
     }
     return {
       insertArr
